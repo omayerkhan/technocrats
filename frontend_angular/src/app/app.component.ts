cf6f510c68c1c1cc1e537589ab670711306e5267
Dileep <dileepkumarreddy9797@gmail.com>
2023-10-14 16:22:16 -0400

    Integrating ngx-webcam package to take plant images and creating basic UI

--- frontend_angular/src/app/app.component.ts
+++ frontend_angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebcamImage, WebcamInitError } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend_angular';
+  showWebcam = true;
+  allowCameraSwitch = true;
+  multipleWebcamsAvailable = false;
+  deviceId: string = '';
+  private trigger: Subject<void> = new Subject<void>();
+  webcamImage: any = null;
+  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
+  diseases: any = [];
+  show: boolean = false;
+  diseaseName: string ='';
+  diseaseDescription: string= '';
+  remedies: string = ''
+  public errors: WebcamInitError[] = [];
+  constructor(){
+
+  }
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  public get nextWebcamObservable(): Observable<boolean|string> {
+    return this.nextWebcam.asObservable();
+  }
+  public showNextWebcam(directionOrDeviceId: boolean|string): void {
+    this.nextWebcam.next(directionOrDeviceId);
+  }
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  public handleImage(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+  }
+  public intializeError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+  public analyze(){
+
+  }
 }
