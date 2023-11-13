755388efef212ec2acd81eef56a9c83c542f8cf4
Omayer Khan <omaayer.khan@gmail.com>
2023-11-12 22:40:10 -0500

    integrating text to speech api

--- frontend_angular/src/app/remedies/remedies.component.ts
+++ frontend_angular/src/app/remedies/remedies.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-remedies',
     templateUrl: './remedies.component.html',
     styleUrls: ['./remedies.component.scss']
   })
-  export class RemediesComponent {
+  export class RemediesComponent implements OnChanges{
+   
     @Input('diseases') diseases: any;
+    @Input('audio') audio: any;
+    @ViewChild('audioElement') audioElement: ElementRef | undefined;
+    ngOnChanges(changes: SimpleChanges): void {
+      this.audioElement?.nativeElement.load();  
+      this.audioElement?.nativeElement.pause();
+    }
+    resetAudio() {
+      this.audioElement?.nativeElement.load();  
+      this.audioElement?.nativeElement.pause();
+    }
   }
+  
